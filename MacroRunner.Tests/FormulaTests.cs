dadb663aa252312e7a5707cedc7e279e3b222017
Vyacheslav Mikhaylov <vmikhaylov@dataart.com>
2021-12-04 01:47:49 +0300

    test were a bit simplified

--- MacroRunner.Tests/FormulaTests.cs
+++ MacroRunner.Tests/FormulaTests.cs
@@ -11,150 +11,137 @@ public class FormulaTests
     [Fact]
     public void ShouldComputeSimpleIntMath()
     {
-        var parser = new FormulaParser(new() { ParametersDelimiter = ',' });
-        var parsed = parser.ParseExpression<int>("1+2*(3+4)");
+        var parsed = CreateParser().ParseExpression<int>("1+2*(3+4)");
         var func = parsed.Compile();
-        
+
         func().Should().Be(15);
     }
-    
+
     [Fact]
     public void ShouldComputeSimpleDoubleMath()
     {
-        var parser = new FormulaParser(new() { ParametersDelimiter = ',' });
-        var parsed = parser.ParseExpression<double>("1.1 + 2.3");
+        var parsed = CreateParser().ParseExpression<double>("1.1 + 2.3");
         var func = parsed.Compile();
-        
+
         func().Should().Be(3.4);
     }
-    
+
     [Fact]
     public void ShouldComputeSimpleMixedMath()
     {
-        var parser = new FormulaParser(new() { ParametersDelimiter = ',' });
-        var parsed = parser.ParseExpression<double>("1 + 2.3");
+        var parsed = CreateParser().ParseExpression<double>("1 + 2.3");
         var func = parsed.Compile();
-        
+
         func().Should().Be(3.3);
     }
-    
+
     [Fact]
     public void ShouldConcatTwoStrings()
     {
-        var parser = new FormulaParser(new() { ParametersDelimiter = ',' });
-        var parsed = parser.ParseExpression<string>("\"aaaa\" + \"bbb\"");
+        var parsed = CreateParser().ParseExpression<string>("\"aaaa\" + \"bbb\"");
         var func = parsed.Compile();
-        
+
         func().Should().Be("aaaabbb");
     }
 
     [Fact]
     public void ShouldComputeScalarFunction()
     {
-        var parser = new FormulaParser(new() { ParametersDelimiter = ',' });
-        var parsed = parser.ParseExpression<double>("sqrt(4)");
+        var parsed = CreateParser().ParseExpression<double>("sqrt(4)");
         var func = parsed.Compile();
-        
+
         func().Should().Be(2);
     }
-    
+
     [Fact]
     public void ShouldComputeScalarFunctionAndConvertToString()
     {
-        var parser = new FormulaParser(new() { ParametersDelimiter = ',' });
-        var parsed = parser.ParseExpression<string>("text(sqrt(4))");
+        var parsed = CreateParser().ParseExpression<string>("text(sqrt(4))");
         var func = parsed.Compile();
-        
+
         func().Should().Be("2");
     }
-    
+
     [Fact]
     public void ShouldComputeScalarFunctionAndConvertToStringWithFormatting()
     {
-        var parser = new FormulaParser(new() { ParametersDelimiter = ',' });
-        var parsed = parser.ParseExpression<string>("text(sqrt(4), \"0000\")");
+        var parsed = CreateParser().ParseExpression<string>("text(sqrt(4), \"0000\")");
         var func = parsed.Compile();
-        
+
         func().Should().Be("0002");
     }
-    
+
     [Fact]
     public void ShouldComputeLogicalFunction()
     {
-        var parser = new FormulaParser(new() { ParametersDelimiter = ',' });
-        var parsed = parser.ParseExpression<bool>("or(true, and(true, false))");
+        var parsed = CreateParser().ParseExpression<bool>("or(true, and(true, false))");
         var func = parsed.Compile();
-        
+
         func().Should().Be(true);
     }
-    
+
     [Fact]
     public void ShouldComputeLogicalFunctionAutoCastToBool()
     {
-        var parser = new FormulaParser(new() { ParametersDelimiter = ',' });
-        var parsed = parser.ParseExpression<bool>("or(1, and(1, 0))");
+        var parsed = CreateParser().ParseExpression<bool>("or(1, and(1, 0))");
         var func = parsed.Compile();
-        
+
         func().Should().Be(true);
     }
-    
+
     [Fact]
     public void ShouldComputeComparisionGreaterThan()
     {
-        var parser = new FormulaParser(new() { ParametersDelimiter = ',' });
-        var parsed = parser.ParseExpression<bool>("(1 + 3) > 0");
+        var parsed = CreateParser().ParseExpression<bool>("(1 + 3) > 0");
         var func = parsed.Compile();
-        
+
         func().Should().Be(true);
     }
-    
-      
+
+
     [Fact]
     public void ShouldComputeComparisionLessThan()
     {
-        var parser = new FormulaParser(new() { ParametersDelimiter = ',' });
-        var parsed = parser.ParseExpression<bool>("(1 + 3) < 0");
+        var parsed = CreateParser().ParseExpression<bool>("(1 + 3) < 0");
         var func = parsed.Compile();
-        
+
         func().Should().Be(false);
     }
-    
+
     [Fact]
     public void ShouldComputeComparisionNotEqual()
     {
-        var parser = new FormulaParser(new() { ParametersDelimiter = ',' });
-        var parsed = parser.ParseExpression<bool>("1 <> 2");
+        var parsed = CreateParser().ParseExpression<bool>("1 <> 2");
         var func = parsed.Compile();
-        
+
         func().Should().Be(true);
     }
-    
+
     [Fact]
     public void ShouldComputeMathFunctionAutoCastFromBool()
     {
-        var parser = new FormulaParser(new() { ParametersDelimiter = ',' });
-        var parsed = parser.ParseExpression<int>("(1 > 0) + (3 = 3)");
+        var parsed = CreateParser().ParseExpression<int>("(1 > 0) + (3 = 3)");
         var func = parsed.Compile();
-        
+
         func().Should().Be(2);
     }
-    
+
     [Fact]
     public void ShouldComputeMathFunctionAutoCastFromBoolMixed()
     {
-        var parser = new FormulaParser(new() { ParametersDelimiter = ',' });
-        var parsed = parser.ParseExpression<double>("(1 > 0) + 3.3");
+        var parsed = CreateParser().ParseExpression<double>("(1 > 0) + 3.3");
         var func = parsed.Compile();
-        
+
         func().Should().Be(4.3);
     }
 
-   
+    private static FormulaParser CreateParser() => new (new() { ParametersDelimiter = ',' });
+
+
     [Fact]
     public void ShouldNotSubtractTwoStrings()
     {
-        var parser = new FormulaParser(new() { ParametersDelimiter = ',' });
-        var exception = Assert.Throws<ParseException>(() => parser.ParseExpression<dynamic>("\"aaaa\" - \"bbb\""));
+        var exception = Assert.Throws<ParseException>(() => CreateParser().ParseExpression<dynamic>("\"aaaa\" - \"bbb\""));
 
         Assert.Contains("Function 'Subtract(String, String)' does not exist.", exception.Message);
     }
